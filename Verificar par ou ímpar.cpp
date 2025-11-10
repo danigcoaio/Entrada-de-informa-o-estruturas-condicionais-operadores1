9921e45a227796d05dc1dd98cf6b563f0aef01db
Daniel Gusmão Coaio <danigcoaio@gmail.com>
2025-11-10 17:26:33 -0300

    Verificar par ou ímpar

---
+++ Verificar par ou ímpar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero; 
+
+    cout << "Digite o número inteiro: " << endl; 
+    cin >> numero; 
+    
+    if(numero % 2 == 0) { 
+        cout << "O número é par: " << endl; 
+} 
+
+    else { 
+        cout << "O número é impar: " << endl; 
+}
+   return 0;
+}
